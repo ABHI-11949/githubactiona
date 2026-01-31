63aa3480be57dce02113c5bd50d98e1e31f39d09
ABHI-11949 <abhishekkumar937287@gmail.com>
2026-01-31 15:51:06 +0530

    Add python scripts

---
+++ abhi.py
@@ -0,0 +1,13 @@
+def sum(a,b):
+    return a+b
+def product(a,b):
+    return a*b
+def difference(a,b):
+    return a-b
+def division(a,b):
+    if b != 0:
+        return a/b
+    else:
+        return "Division by zero is not allowed"
+def modulus(a,b):
+    return a%b
---
+++ in.py
@@ -0,0 +1,7 @@
+import abhi
+print(abhi.sum(5,3))
+
+print(abhi.product(5,3))
+print(abhi.difference(5,3))
+print(abhi.division(5,0))
+print(abhi.modulus(5,3))
